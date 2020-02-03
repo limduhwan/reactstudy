3ca03cd8aef48a0f68f9669f3244958613001e38
limduhwan <limduhwan@gmail.com>
2020-02-03 10:49:42 +0900

    200201

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,16 +1,46 @@
 import * as types from '../actions/ActionTypes';
 
 const initialState = {
-    color: 'black',
-    number: 0
+    counters: [
+        {
+            color: 'black',
+            number: 0
+        }
+    ]
 };
 
 function counter(state = initialState, action) {
+
+    const { counters } = state;
+
     switch (action.type) {
+        case types.CREATE:
+            return {
+              counters: [
+                  ...counters,
+                  {
+                      color: action.color,
+                      number: 0
+                  }
+              ]
+            };
+
+        case types.REMOVE:
+            return {
+                counters: counters.slice(0, counters.length - 1)
+
+            };
         case types.INCREMENT:
             return {
-                ...state,
-                number: state.number + 1
+                counters: [
+                    ...counters.slice(0, action.index),
+                    {
+                        ...counters[action.index],
+                        number: counters[action.index].number +1
+                    },
+                    ...counters.slice(action.index + 1, counters.length)
+                ]
+
             };
         case types.DECREMENT:
             return {
