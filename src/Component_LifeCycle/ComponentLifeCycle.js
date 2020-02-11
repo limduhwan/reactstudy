022b32e42fcc38ea83499c260df83f8923c9cea0
limduhwan <limduhwan@gmail.com>
2020-02-11 18:57:15 +0900

    Epic menu

---
+++ src/Component_LifeCycle/ComponentLifeCycle.js
@@ -0,0 +1,5 @@
+// 컴포넌트를 생성할 때는
+// constructor -> componentWillMount -> render -> componentDidMount
+
+// 컴포넌트의 prop이 변경될 때는
+// omponentWillReceiveProps -> shouldComponentUpdate -> componentWillUpdate-> render -> componentDidUpdate
